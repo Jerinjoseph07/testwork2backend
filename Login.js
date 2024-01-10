e4ec64c5038a1709ace25bd341f56cf440f07915
Jerinjoseph07 <jerinjoseph077@gmail.com>
2024-01-11 01:14:06 +0530

    first commit

---
+++ Login.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose")
+mongoose.connect("mongodb+srv://jerinjoseph077:kollanur@cluster0.hvridew.mongodb.net/worker1?retryWrites=true&w=majority")
+.then(()=>{
+    console.log("mongodb connected");
+})
+.catch(()=>{
+    console.log('failed');
+})
+
+
+const newSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const collection = mongoose.model("collection",newSchema)
+
+module.exports=collection
